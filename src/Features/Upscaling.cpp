8b2a7bea8ff88c1785210b0de9b96b200b2fcea9
Alan Tse <alandtse@gmail.com>
2025-09-27 03:20:54 -0700

    chore: fix spelling

--- src/Features/Upscaling.cpp
+++ src/Features/Upscaling.cpp
@@ -752,7 +752,7 @@ void Upscaling::ConfigureUpscaling(RE::BSGraphics::State* a_viewport)
 	dynamicResolutionWidthRatio = resolutionScale.x;
 	dynamicResolutionHeightRatio = resolutionScale.y;
 
-	// Disable dynamic resolution unless the game explictly enables it
+	// Disable dynamic resolution unless the game explicitly enables it
 	if (!globals::game::isVR)
 		runtimeData.dynamicResolutionLock = 1;
 
